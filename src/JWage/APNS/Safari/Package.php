cf5fdf40421d916a330853424b04029bc36fddcb
lrusev <rusev.leonid@gmail.com>
2017-01-11 14:46:44 +0200

    Added 256, 512 icons to the paackage list, requires for Safari 10

--- src/JWage/APNS/Safari/Package.php
+++ src/JWage/APNS/Safari/Package.php
@@ -14,6 +14,10 @@ class Package
         'icon.iconset/icon_32x32@2x.png',
         'icon.iconset/icon_128x128.png',
         'icon.iconset/icon_128x128@2x.png',
+        'icon.iconset/icon_256x256.png',
+        'icon.iconset/icon_256x256@2x.png',
+        'icon.iconset/icon_512x512.png',
+        'icon.iconset/icon_512x512@2x.png',
         'website.json'
     );
 
